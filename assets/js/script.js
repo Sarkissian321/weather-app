f0fa799f47d334abc02a88b3f07d0002b2231a57
Meline Sarkissian <sarkissian.mell321@yahoo.com>
2023-10-12 18:25:58 -0700

    bootstrap successfully installed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+localStorage.clear();
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=ab9ea777839e319813126c72df76177";
+var searchBtn = document.getElementById("search-btn");
+var cityInput = document.getElementById("city-input");
+var weatherDashboard = document.getElementById("dashboard");
+searchBtn.addEventListener("click", searchCity);
 
-function createWeatherCard(cityName, weatherData) {
-    const weatherCardsDiv = document.querySelector('.weather-cards');
-    const card = document.createElement('div');
+function searchCity() {
+    
 }
-
